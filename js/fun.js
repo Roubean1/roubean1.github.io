25ed8a25d74c8a78d196a0532b256ce538e52d87
Roubean1 <65488270+Roubean1@users.noreply.github.com>
2023-11-11 00:45:10 +0100

    teď by to mělo být responzivní až na tu pohormu jménem mathjax, která to bohužel nedovolí udělat na 100% pěkné

--- js/fun.js
+++ js/fun.js
@@ -4,18 +4,20 @@ var content = document.getElementById("content");
 
 var isSidebarOpen = false;
 
-toggleBtn.addEventListener("click", function() {
+toggleBtn.addEventListener("click", function () {
     if (!isSidebarOpen) {
+        // Code to open the sidebar
         sidebar.style.display = "block";
-        setTimeout(function() {
+        setTimeout(function () {
             sidebar.style.opacity = "1";
-            content.style.marginLeft = "310px"; // Posunutí obsahu
+            content.style.marginLeft = "300px"; // Posunutí obsahu
             content.style.paddingLeft = "20px";
         }, 10);
         isSidebarOpen = true;
     } else {
+        // Code to close the sidebar
         sidebar.style.opacity = "0";
-        setTimeout(function() {
+        setTimeout(function () {
             sidebar.style.display = "none";
             content.style.marginLeft = "0"; // Nulování marginu pro vrácení obsahu zpět
         }, 300);
@@ -23,18 +25,17 @@ toggleBtn.addEventListener("click", function() {
     }
 });
 
-    var xhr = new XMLHttpRequest();
-    var aktualniCesta = window.location.search;
-    if(aktualniCesta.length == 0){
-        aktualniCesta = 'uvod.txt';
-    }
-    else {
-        aktualniCesta = aktualniCesta.slice(1);
-    }
-    xhr.open("GET", aktualniCesta, true);
-    xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-    document.getElementById("obsah").innerHTML = xhr.responseText;
+var xhr = new XMLHttpRequest();
+var aktualniCesta = window.location.search;
+if (aktualniCesta.length == 0) {
+    aktualniCesta = 'uvod.txt';
+} else {
+    aktualniCesta = aktualniCesta.slice(1);
 }
+xhr.open("GET", aktualniCesta, true);
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        document.getElementById("obsah").innerHTML = xhr.responseText;
+    }
 };
-    xhr.send();
+xhr.send();
